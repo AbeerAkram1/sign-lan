46f495694459ac84ae44aef3db4220bf26783fad
abeer akram <abeerakram567@gmail.com>
2025-02-28 14:16:19 +0500

    Normalize line endings

---
+++ src/quiz/data/objects.js
@@ -0,0 +1,36 @@
+export const objectData = {
+  easy: [
+    { id: "A", object: "Apple", image: "/objects/apple.jpeg" },
+    { id: "B", object: "Ball", image: "/objects/ball.png" },
+    { id: "C", object: "Cat", image: "/objects/cat.png" },
+    { id: "D", object: "Dog", image: "/objects/dog.png" },
+    { id: "E", object: "Elephant", image: "/objects/elephant.png" },
+  ],
+  medium: [
+    { id: "F", object: "Fish", image: "/objects/fish.png" },
+    { id: "G", object: "Grapes", image: "/objects/grapes.png" },
+    { id: "H", object: "Hat", image: "/objects/hat.png" },
+    { id: "I", object: "Ice Cream", image: "/objects/ice-cream.png" },
+    { id: "J", object: "Jug", image: "/objects/jug.jpg" },
+    { id: "K", object: "Koala", image: "/objects/koala.avif" },
+    { id: "L", object: "Lion", image: "/objects/lion.webp" },
+  ],
+  hard: [
+    { id: "M", object: "Mango", image: "/objects/mango.png" },
+    { id: "N", object: "Nest", image: "/objects/nest.png" },
+    { id: "O", object: "Orange", image: "/objects/orange.png" },
+    { id: "P", object: "Parrot", image: "/objects/parrot.png" },
+    { id: "Q", object: "Quokka", image: "/objects/quokka.jpeg" },
+    { id: "R", object: "Rabbit", image: "/objects/rabbit.png" },
+    { id: "S", object: "Sun", image: "/objects/sun.png" },
+    { id: "T", object: "Tree", image: "/objects/tree.png" },
+    { id: "U", object: "Umbrella", image: "/objects/umbrella.jpeg" },
+    { id: "V", object: "Violin", image: "/objects/violin.png" },
+    { id: "W", object: "Watch", image: "/objects/watch.jpeg" },
+    { id: "X", object: "Xylophone", image: "/objects/Xylophone.png" },
+    { id: "Y", object: "Yacht", image: "/objects/Yacht.jpeg" },
+    { id: "Z", object: "Zebra", image: "/objects/zebra.jpeg" },
+  ],
+};
+
+export default objectData;
